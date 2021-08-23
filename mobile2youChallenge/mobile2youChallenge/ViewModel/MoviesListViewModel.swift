5d68fe4215448aedc0dde27a7c37a6f56902e74d
ProKevinRibeiro <49958388+ProKevinRibeiro@users.noreply.github.com>
2021-08-22 22:05:28 -0300

    Set some viewModel configs
    Push API data from vm and delete MovieModel(it's not necessary)

--- mobile2youChallenge/mobile2youChallenge/ViewModel/MoviesListViewModel.swift
+++ mobile2youChallenge/mobile2youChallenge/ViewModel/MoviesListViewModel.swift
@@ -9,11 +9,34 @@ import Foundation
 import RxSwift
 
 protocol MoviesListViewModelDelegate {
-    
+    func didChangeIsMovieLiked()
 }
 
 struct MoviesListViewModel {
     
-    let delegate: MoviesListViewController?
+    let movieIdInDisplay: Int = 18 //movie chosed
+    var isMovieLiked: Bool = false
+    let delegate: MoviesListViewModelDelegate?
    
+    func getPrincipalMovie() -> Observable<MovieDetails> {
+        return ServiceClient.getMovieDetails(id: movieIdInDisplay)
+    }
+    
+    func getSimilarMovies() -> Observable<SimilarMovies> {
+        return ServiceClient.getSimilarMovies(id: movieIdInDisplay)
+    }
+    
+    func getImageByPath(path: String) -> UIImage {
+        let url = URL.init(fileURLWithPath: path)
+        
+        let imageData:NSData = NSData(contentsOf: url)!
+
+        return UIImage(data: imageData as Data) ?? UIImage(systemName: "film")!
+    }
+    
+    mutating func changeIsMovieLiked() {
+        isMovieLiked = !isMovieLiked
+        delegate?.didChangeIsMovieLiked()
+    }
 }
+
