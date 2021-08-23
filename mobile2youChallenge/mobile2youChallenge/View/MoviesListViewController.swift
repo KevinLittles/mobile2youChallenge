63506daeb4a7142e9e3a21de2e176458c2e2d5bc
ProKevinRibeiro <49958388+ProKevinRibeiro@users.noreply.github.com>
2021-08-23 00:13:32 -0300

    movieTitle loading data is okay

--- mobile2youChallenge/mobile2youChallenge/View/MoviesListViewController.swift
+++ mobile2youChallenge/mobile2youChallenge/View/MoviesListViewController.swift
@@ -96,6 +96,28 @@ extension MoviesListViewController: ViewCodeConfiguration {
         
         func setupMovieTitle() {
             
+            viewModel.getPrincipalMovie()
+                .observe(on: MainScheduler.instance)
+                .subscribe { movieDetails in
+                    
+                    self.movieTitle.text = movieDetails.title
+                    
+                } onError: { error in
+                    switch error {
+                    case ServiceError.conflict:
+                        print("Conflict error")
+                    case ServiceError.forbidden:
+                        print("Forbidden error")
+                    case ServiceError.notFound:
+                        print("Not found error")
+                    default:
+                        print("Unknown error:", error)
+            
+                    }
+                }
+                .disposed(by: disposeBag)
+            
+            movieTitle.textColor = .white
         }
         
         func setupTopLikeButtom() {
